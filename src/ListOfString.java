6eb6a4eaab51d04c051b6b9c66088f74faf67b8a
Adam Smith <apsmith@ccs.neu.edu>
2019-08-07 20:40:57 -0400

    Modifications to DataSet

--- src/ListOfString.java
+++ src/ListOfString.java
@@ -1,15 +1,22 @@
 import javax.xml.crypto.Data;
 import java.util.ArrayList;
+import java.util.Map;
 
-public class ListOfString implements Data {
-    private DataSet dataSet = (DataSet) new ArrayList<String>();
+public class ListOfString implements DataSet {
+    private DataSet dataSet;
     // TODO: Ask the team: Is this structure okay, or should be something else?
 
-
-    DataSet getDataset() {
-        return dataSet;
+    // this initializer is used by the User to provide raw data
+    public void DataSet(ArrayList<String> dataSet) {
+        this.dataSet = (DataSet) dataSet;
     }
-    void setDataSet(DataSet dataSet) {
-        this.dataSet = dataSet;
+
+    // this initializer is used by the TaskManager and JobManager
+    // to return the outcome of the Map and Reduce functions
+    public void DataSet(Map<String, Integer> dataSet) {
+        this.dataSet = (DataSet) dataSet;
+    }
+    public DataSet getDataset() {
+        return dataSet;
     }
 }
